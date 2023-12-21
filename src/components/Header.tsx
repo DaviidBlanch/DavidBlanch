86fc959a195bcd5b3184d47c6615114451342e81
David Blanch Bellido <dablabe98@gmail.com>
2023-12-21 22:13:57 +0100

    Fix Linkedin image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { NavLink as NavL } from 'react-router-dom'
 
 import GitHub from '../assets/socialNetwork-logos/GitHub-logo.webp'
 import Footer from './Footer'
+import LinkedIn from '../assets/socialNetwork-logos/LinkedIn'
 
 export default function Header() {
     return (
@@ -23,7 +24,7 @@ export default function Header() {
                             <img
                                 src={GitHub}
                                 alt='Github logo'
-                                className='w-full h-6 hover:scale-110 transition-transform duration-300'
+                                className='w-6 h-6 hover:scale-110 transition-transform duration-300'
                             />
                         </a></li>
                         <li><a
@@ -31,11 +32,12 @@ export default function Header() {
                             target='_blank'
                             rel='noreferrer'
                         >
-                            <img
+                            {/* <img
                                 src='src/assets/socialNetwork-logos/LinkedIn.svg'
                                 alt='LinkedIn logo'
                                 className='w-full h-6 hover:scale-110 transition-transform duration-300'
-                            />
+                            /> */}
+                            <LinkedIn />
                         </a></li>
                     </ul>
                 </nav>
