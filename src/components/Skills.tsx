753b311a8616ecb272acf9e73c8e6d30ea5e6e60
David Blanch Bellido <dablabe98@gmail.com>
2023-12-21 23:14:45 +0100

    Fix skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,27 +1,24 @@
-import { useEffect, useState } from "react"
-import { SkillResponse, SkillsItems } from "../interfaces/skills"
-
 import '../styles/skills.css'
 
-const URL_SKILLS = "https://run.mocky.io/v3/dffcd1d1-668d-4d03-957d-7f9140706cd8"
+// const URL_SKILLS = "https://run.mocky.io/v3/dffcd1d1-668d-4d03-957d-7f9140706cd8"
 
 export default function Skills() {
 
-    const [skillData, setSkillData] = useState<SkillsItems[]>()
+    // const [skillData, setSkillData] = useState<SkillsItems[]>()
 
-    useEffect(() => {
-        fetch(URL_SKILLS)
-            .then(res => res.json())
-            .then((data: SkillResponse) => {
-                if (!data) return
-                setSkillData(data.skills)
-            })
-            .catch(error => console.error(error))
-    }, [])
+    // useEffect(() => {
+    //     fetch(URL_SKILLS)
+    //         .then(res => res.json())
+    //         .then((data: SkillResponse) => {
+    //             if (!data) return
+    //             setSkillData(data.skills)
+    //         })
+    //         .catch(error => console.error(error))
+    // }, [])
 
     return (
         <div className="p-2 pt-0 mb-20 lg:pl-10">
-            {
+            {/* {
                 skillData?.map(skill => (
                     <div key={skill.id} className="mb-3">
                         <div className="flex justify-between mb-1">
@@ -36,7 +33,32 @@ export default function Skills() {
                         </div>
                     </div>
                 ))
-            }
+            } */}
+
+            <div className="mb-3">
+                <div className="flex justify-between mb-1">
+                    <img src="src/assets/frameworks-logos/React.svg" alt="ReactJs logo" />
+                    <span className={`text-sm font-medium`} style={{ color: "#087EA4" }}>70%</span>
+                </div>
+                <div className="w-full bg-gray-200 rounded-full h-2.5">
+                    <div
+                        className={`h-2.5 rounded-full animated-progress-bar`}
+                        style={{ width: "70%", backgroundColor: "#087EA4" }}
+                    />
+                </div>
+            </div>
+            <div className="mb-3">
+                <div className="flex justify-between mb-1">
+                    <img src="src/assets/frameworks-logos/TypeScript.svg" alt="TypeScript logo" />
+                    <span className={`text-sm font-medium`} style={{ color: "#3178c6" }}>70%</span>
+                </div>
+                <div className="w-full bg-gray-200 rounded-full h-2.5">
+                    <div
+                        className={`h-2.5 rounded-full animated-progress-bar`}
+                        style={{ width: "70%", backgroundColor: "#3178c6" }}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
